459d26262bca3fb05c668a369ca1ee9b3094ee46
uxin1001 <1329220172@qq.com>
2020-11-01 18:13:10 +0800

    首页国际化修改

---
+++ src/main/java/com/xin/staffweb/config/MyLocaleResolver.java
@@ -0,0 +1,39 @@
+package com.xin.staffweb.config;
+
+import org.springframework.web.servlet.LocaleResolver;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class MyLocaleResolver implements LocaleResolver {
+
+    // 解析请求
+    @Override
+    public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+
+        // 获取语言中的请求参数
+        String language = httpServletRequest.getParameter("l");
+        Locale locale = Locale.getDefault(); //如果没有就使用默认的
+
+        // 如果请求携带了国际化的参数
+        if(!StringUtils.isEmpty(language)){
+
+            // zh_CN
+            String[] split = language.split("_");
+            // 国家，地区
+            locale = new Locale(split[0], split[1]);
+
+
+        }
+
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+
+}
