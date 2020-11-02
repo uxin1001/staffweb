ffeca44d834737af997bae39786e1689fbd40f99
uxin <uxin@163.com>
2020-11-02 14:16:59 +0800

    拦截器

--- src/main/java/com/xin/staffweb/config/MyMvcConfig.java
+++ src/main/java/com/xin/staffweb/config/MyMvcConfig.java
@@ -3,6 +3,7 @@ package com.xin.staffweb.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +21,12 @@ public class MyMvcConfig implements WebMvcConfigurer{
     @Bean
     public LocaleResolver localeResolver(){
         return new MyLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html","/","/user/login","/css/**","/js/**","/img/**");
     }
 }
--- src/main/java/com/xin/staffweb/controller/LoginController.java
+++ src/main/java/com/xin/staffweb/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.xin.staffweb.controller;
 
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
@@ -14,10 +16,12 @@ public class LoginController {
     public String login(
             @RequestParam("username") String username,
             @RequestParam("password") String password,
-            Model model){
+            Model model, HttpSession session){
 
         // 具体业务
         if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+
+            session.setAttribute("loginUser",username);
             return "redirect:/main.html";
         }else {
             // 告诉用户登录失败
