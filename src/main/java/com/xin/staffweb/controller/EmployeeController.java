73ec793262f3ccc72bf29f221027e8473dc7af69
uxin <uxin@163.com>
2020-11-12 16:06:00 +0800

    修改

--- src/main/java/com/xin/staffweb/controller/EmployeeController.java
+++ src/main/java/com/xin/staffweb/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,5 +43,19 @@ public class EmployeeController {
         employeeDao.save(employee);//调用底层业务方法保存员工信息
 
         return "redirect:/emps";
+    }
+
+    // 跳转到修改员工界面
+    @GetMapping("/emp/{id}")
+    public String toUpdateEmp(@PathVariable("id")Integer id, Model model){
+        //查出原来数据
+        Employee employee = employeeDao.getEmployeeById(id);
+        model.addAttribute("emp",employee);
+
+
+        // 查出所有部门信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emp/update";
     }
 }
